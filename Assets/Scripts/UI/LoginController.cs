ed971c63cf2467c420f30d85ac9da83bd9acb020
Vladislav Smirnov <beluckydaf@gmail.com>
2020-06-22 22:42:01 +0300

    Login restore.

--- Assets/Scripts/UI/LoginController.cs
+++ Assets/Scripts/UI/LoginController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Doozy.Engine;
 using Managers;
 using TMPro;
 using UnityEngine;
@@ -12,6 +14,13 @@ namespace UI
         [SerializeField] private TMP_InputField _emailField;
         [SerializeField] private TMP_InputField _passwordField;
 
+        public void TryRestore()
+        {
+            GameEventMessage.SendEvent(ManagerContainer.Instance.SessionManager.IsLoggedIn()
+                ? "UserLoginRestored"
+                : "UserLoginNeeded");
+        }
+
         /// <summary>
         /// Uses UI data to call login from session manager.
         /// </summary>
@@ -19,6 +28,15 @@ namespace UI
         {
             Assert.IsNotNull(ManagerContainer.Instance);
             ManagerContainer.Instance.SessionManager.LoginEmail(_emailField.text, _passwordField.text);
+        }
+        
+        /// <summary>
+        /// Uses session manager to log out.
+        /// </summary>
+        public void Logout()
+        {
+            Assert.IsNotNull(ManagerContainer.Instance);
+            ManagerContainer.Instance.SessionManager.LogOut();
         }
     }
 }
